e4d3f7553dad4af32c64a39ac6cf17c962677f28
vndrd <ivangddzzz@gmail.com>
2020-07-25 22:31:48 -0400

    inicio

---
+++ src/components/Catalogo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Catalogo</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Catalogo',
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Catalogo msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Catalogo from '@/components/Catalogo.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Catalogo
+  }
+}
+</script>
